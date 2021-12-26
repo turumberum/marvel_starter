f3d1e7051d6b63de404b76dfceb35d06c7ec4878
turumberum <72048072+turumberum@users.noreply.github.com>
2021-12-26 22:51:51 +0300

    heroku

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
 
-// import MarvelService from './services/MarvelService';
+    // import MarvelService from './services/MarvelService';
 
-// const marvelService = new MarvelService();
+    // const marvelService = new MarvelService();
 
-// marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
-// marvelService.getCharacter(1011052).then(res => console.log(res))
+    // marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
+    // marvelService.getCharacter(1011052).then(res => console.log(res))
 
 ReactDOM.render(
   <React.StrictMode>
